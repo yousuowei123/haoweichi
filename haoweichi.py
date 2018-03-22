c2b7275ae666d99795524886163d52df4bc7aac1
ctob <916643967@qq.com>
2018-03-23 00:24:20 +0800

    搭建脚本框架

--- haoweichi.py
+++ haoweichi.py
@@ -1,4 +1,23 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # -*- author: c_tob -*-
+import requests
+from bs4 import BeautifulSoup
 
+
+def get_page_source(url):
+	pass
+
+
+def parse_page_source(html):
+	pass
+
+
+def main():
+	url = 'http://www.haoweichi.com/Index/random'
+	html = get_page_source(url)
+	results = parse_page_source(html)
+
+
+if __name__ == "__main__":
+	main()
